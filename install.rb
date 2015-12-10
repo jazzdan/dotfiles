839cb78638c69003c3293ac902ff0b0961c11f12
dmiller <dmiller@etsy.com>
2015-12-10 16:19:13 +0000

    make the install script a bit more robust

--- install.rb
+++ install.rb
@@ -6,3 +6,13 @@ Dir.foreach('.') do |item|
     next if item == '.' or item == '..' or item == 'install.rb' or item == 'README.md'
     FileUtils.ln_s(Dir.pwd + '/' + item, Dir.home + '/' + item, :force => true)
 end
+
+`source ~/.bashrc`
+
+# install Vundle, if not already installed
+if !File.directory?('~/.vim/bundle/Vundle.vim')
+    `git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim`
+end
+
+# Do the vundle install
+`vim +PluginInstall +qall`
