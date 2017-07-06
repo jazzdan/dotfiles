e195dc4c807ecff90dd86edcccbf91554084092b
Dan Miller <dan@dmiller.io>
2017-07-06 14:23:45 -0400

    update dotfiles installer script

--- install.rb
+++ install.rb
@@ -2,9 +2,20 @@
 
 require 'fileutils'
 require 'set'
+require 'optparse'
+
+options = {}
+
+OptionParser.new do |opts|
+    opts.banner = "\nDotfile installer\n"
+    
+    opts.on("-d", "--dry-run", "Does a dry run") do |d|
+        options[:dryrun] = d
+    end
+end.parse!
 
 files_to_skip = Set.new([
-    '.', '..', 'install.rb', 'README.md'
+    '.', '..', 'install.rb', 'README.md', '.git'
 ])
 
 file_to_destination_map = {
@@ -14,13 +25,20 @@ file_to_destination_map = {
 
 Dir.foreach('.') do |item|
     next if files_to_skip.include? item
-
+    
     path_to_copy_to = Dir.home + '/' + item
     if file_to_destination_map.include? item
-        path_to_copy_to = file.file_to_destination_map[item]
+        path_to_copy_to = file_to_destination_map[item]
     end
 
-    FileUtils.ln_s(Dir.pwd + '/' + item, path_to_copy_to, :force => true)
+    if options[:dryrun] === true
+        puts "ln -s #{Dir.pwd + '/' + item} #{path_to_copy_to}"
+    else 
+        if !File.directory? path_to_copy_to
+            FileUtils.mkdir_p(path_to_copy_to)
+        end
+        FileUtils.ln_s(Dir.pwd + '/' + item, path_to_copy_to, :force => true)
+    end
 end
 
 #`source ~/.bashrc`
